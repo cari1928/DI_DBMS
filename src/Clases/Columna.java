db45231fe68ba50a263e55bc9ddca1abc1844422
cari1928 <13030622@itcelaya.edu.mx>
2017-03-05 08:38:41 -0600

    Inicio Proyecto

---
+++ src/Clases/Columna.java
@@ -0,0 +1,16 @@
+package Clases;
+
+/**
+ *
+ * @author Tenistas
+ */
+public class Columna {
+
+    int tabid;
+    int colid;
+    char[] nomcol = new char[10];
+    int coltam;
+    int tabref;
+    int colref;
+
+}
---
+++ src/Clases/Tabla.java
@@ -0,0 +1,24 @@
+package Clases;
+
+import java.util.List;
+
+/**
+ *
+ * @author Tenistas
+ */
+public class Tabla {
+
+    List<Columna> listColumnas;
+    List<Indice> listIndices;
+    String nombtab;
+    String archivo;
+    int tabid;
+    int tamreng;
+    int ncols;
+    double nrengs;
+    int nindex;
+
+    public void verificaNumeroIndices() {
+        //código
+    }
+}
